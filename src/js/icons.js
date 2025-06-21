0c94acdf1a2e31d2141ffb09117c91ec6ec4f022
rodrigoespigares <rodrigoespigaresfernandez@gmail.com>
2025-06-21 23:30:22 +0200

    nuevo icono de dolar

--- src/js/icons.js
+++ src/js/icons.js
@@ -72,7 +72,7 @@ const archivosydocumentos = {
         <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
         <polyline points="17,8 12,3 7,8"/>
         <line x1="12" x2="12" y1="3" y2="15"/>
-    </svg>`,        
+    </svg>`,
 
     'folder': `<svg class="icon-svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
         <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"/>
@@ -142,10 +142,14 @@ const comercio = {
         <rect x="3" y="3" width="18" height="18" rx="2" ry="2"/>
         <path d="M12 8v8M8 12h8"/>
         </svg>`,
-    
+
     'invoice': `<svg class="icon-svg" viewBox="0 0 16 16"><path fill="#000000" d="M4.4 10.2c-.6.1-1.4-.3-1.7-.4l-.5.9s.9.4 1.7.5v.8h1v-.9c.9-.3 1.4-1.1 1.5-1.8c0-.8-.6-1.4-1.9-1.9c-.4-.2-1.1-.5-1.1-.9c0-.5.4-.8 1-.8c.7 0 1.4.3 1.4.3l.4-.9s-.5-.2-1.2-.4V4H4v.7c-.9.2-1.5.8-1.6 1.7c0 1.2 1.3 1.7 1.8 1.9c.6.2 1.3.6 1.3.9c0 .4-.4.9-1.1 1"/><path fill="#000000" d="M0 2v12h16V2zm15 11H1V3h14z"/><path fill="#000000" d="M8 5h6v1H8zm0 2h6v1H8zm0 2h3v1H8z"/></svg>`,
 
     'ticket': `<svg class="icon-svg" viewBox="0 0 24 24"><path fill="none" stroke="#000000" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3h14v18l-1.032-.884a2 2 0 0 0-2.603 0L14.333 21l-1.031-.884a2 2 0 0 0-2.604 0L9.667 21l-1.032-.884a2 2 0 0 0-2.603 0L5 21zm10 4H9m6 4H9m6 4h-4"/></svg>`,
+
+    'currency-dollar': `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-currency-dollar" viewBox="0 0 16 16">
+        <path d="M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z"/>
+    </svg>`,
 }
 
 
